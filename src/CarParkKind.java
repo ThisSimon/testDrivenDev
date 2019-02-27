d064b6306233090c1f88f6132d3e8fb78117f8a5
ThisSimon <c00208558@itcarlow.ie>
2019-02-27 19:15:37 +0000

    changed CarParkKind enum to contain variables

--- src/CarParkKind.java
+++ src/CarParkKind.java
@@ -3,6 +3,41 @@ import java.math.BigDecimal;
 /**
  * Created by CM on 01/02/2018.
  */
-public enum CarParkKind {
-    STAFF,STUDENT,MANAGMENT,VISITOR;
+public enum CarParkKind {STAFF("staff", 0.00, 1.00, 0.00, 16.00),
+    STUDENT("student", 5.50, 0.25, 0.00, 0.00),
+    MANAGEMENT("management", 0.00, 1.00, 3.00, 0.00),
+    VISITOR("visitor", 8.00, 0.50, 0.00, 0.00);
+    private final String name;
+    private final double free;
+    private final double reduction;
+    private final double minimum;
+    private final double maximum;
+
+    CarParkKind(String name, double free, double reduction, double minimum, double maximum) {
+        this.name = name;
+        this.free = free;
+        this.reduction = reduction;
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getFree() {
+        return this.free;
+    }
+
+    public double getReduction() {
+        return this.reduction;
+    }
+
+    public double getMinimum() {
+        return this.minimum;
+    }
+
+    public double getMaximum() {
+        return this.maximum;
+    }
 }
